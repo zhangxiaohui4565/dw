0e2e1a1e430179aa3c9ff092549b50889ee6b687
zhangxiaohui <zhangxiaohui@xkw.cn>
2020-09-18 11:56:30 +0800

    chore: for file name

---
+++ offline/src/main/resources/sql/dwd/create_dwd_start_log.sql
@@ -0,0 +1,30 @@
+drop table if exists gmall.dwd_start_log;
+CREATE EXTERNAL TABLE gmall.dwd_start_log(
+`mid_id` string,
+`user_id` string,
+`version_code` string,
+`version_name` string,
+`lang` string,
+`source` string,
+`os` string,
+`area` string,
+`model` string,
+`brand` string,
+`sdk_version` string,
+`gmail` string,
+`height_width` string,
+`app_time` string,
+`network` string,
+`lng` string,
+`lat` string,
+`entry` string,
+`open_ad_type` string,
+`action` string,
+`loading_time` string,
+`detail` string,
+`extend1` string
+)
+PARTITIONED BY (dt string)
+stored as orc
+location '/warehouse/gmall/dwd/dwd_start_log/'
+TBLPROPERTIES ("orc.compress"="SNAPPY");
