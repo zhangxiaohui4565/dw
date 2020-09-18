952868caadc24b7845774b24f4a9d7d36e2cea7a
zhangxiaohui <zhangxiaohui@xkw.cn>
2020-09-18 17:03:04 +0800

    Feat: 增加执行脚本

---
+++ offline/src/main/bin/ods_log.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source /etc/profile
+# 定义变量方便修改
+APP=gmall
+
+# 如果是输入的日期按照取输入日期；如果没输入日期取当前时间的前一天
+if [ -n "$1" ] ;then
+   do_date=$1
+else
+   do_date=`date -d "-1 day" +%F`
+fi
+
+echo "===日志日期为 $do_date==="
+sql="
+load data inpath '/origin_data/gmall/log/topic_start/$do_date' into table "$APP".ods_start_log partition(dt='$do_date');
+
+load data inpath '/origin_data/gmall/log/topic_event/$do_date' into table "$APP".ods_event_log partition(dt='$do_date');
+"
+
+# 指定使用hive 用户执行脚本提交任务
+/usr/bin/beeline -u "jdbc:hive2://node01:10000/" -n hive -p hive -e "$sql"
+
