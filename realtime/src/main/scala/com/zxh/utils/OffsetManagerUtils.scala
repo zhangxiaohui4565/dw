14ffd23b8bf244067eca4ce8d680d827745f6213
zhangxiaohui4565 <zhangxh940426@163.com>
2020-11-10 08:58:51 +0800

    Feat： 矫正偏移量代码

---
+++ realtime/src/main/scala/com/zxh/utils/OffsetManagerUtils.scala
@@ -0,0 +1,59 @@
+package com.zxh.utils
+
+import java.time.Duration
+import java.util
+
+import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer, NoOffsetForPartitionException}
+import org.apache.kafka.common.TopicPartition
+
+import scala.collection.mutable
+import scala.collection.mutable.Map
+
+/**
+  * @author zhangxh
+  * @date 2020/11/10 8:56
+  * @version 1.0
+  */
+object OffsetManagerUtils {
+  
+  private def getEarliestOffsets(kafkaParams: Map[String, Object], topics: List[String]) = {
+    val newKafkaParams = mutable.Map[String, Object]()
+    newKafkaParams ++= kafkaParams
+    newKafkaParams.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+
+    //kafka api
+    newKafkaParams.toMap.asInstanceOf[java.util.Map[String, Object]]
+    val consumer = new KafkaConsumer(newKafkaParams.toMap.asInstanceOf[java.util.Map[String, Object]])
+    //订阅
+    import scala.collection.JavaConverters._
+    val topic = topics.asJava
+    consumer.subscribe(topic)
+    val noOffsetForPartitionExceptionSet = mutable.Set()
+    try
+      consumer.poll(Duration.ZERO)
+    catch {
+      case e: NoOffsetForPartitionException =>
+      //        noOffsetForPartitionExceptionSet.add(e.partition())
+      //邮件报警
+    }
+    val partitions: util.Set[TopicPartition] = consumer.assignment()
+    //获取 分区信息
+    val topicp = partitions
+    //暂定消费
+    consumer.pause(topicp)
+    //从头开始
+    consumer.seekToBeginning(topicp)
+    // key 为new TopicPartition(topic.partition)
+    //position(TopicPartition partition)
+    var topicOffsetMap = Map[TopicPartition, Long]()
+    for (tp <- topicp) {
+      val tp: TopicPartition = tp
+      // Get the offset of the <i>next record</i>
+      val pOffset: Long = consumer.position(tp)
+      topicOffsetMap(tp) = pOffset
+    }
+    consumer.unsubscribe()
+    consumer.close()
+    topicOffsetMap
+  }
+}
