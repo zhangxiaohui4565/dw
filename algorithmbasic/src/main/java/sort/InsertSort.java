ee7db0c9db2650682ec85fc84e70d3a66750bb9f
zhangxiaohui <zhangxiaohui@xkw.cn>
2020-12-03 08:39:50 +0800

    study: InsertSort

---
+++ algorithmbasic/src/main/java/sort/InsertSort.java
@@ -0,0 +1,32 @@
+package sort;
+
+/**
+ * @author zhangxh
+ * @version 1.0
+ * @date 2020/12/3 8:30
+ */
+public class InsertSort {
+    public static void insertSort(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j-1] > arr[j]){
+                    swap(arr,j-1,j);
+                }
+            }
+        }
+    }
+
+    private static void swap(int arr[], int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] test = {1, 2, 2, 7, 5, 4, 3, 4,32,32,4,4,2,2,9};
+        System.out.println(test.toString());
+//        mergeSort1(test);
+        insertSort(test);
+        System.out.println(test.toString());
+    }
+}
